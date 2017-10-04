ca471d0a5a9a9e3360bf5607588c71508110db12
udithv <udithv01@gmail.com>
2017-10-04 11:53:03 +0530

    Created Endpoint

--- server.js
+++ server.js
@@ -1,17 +1,39 @@
 const express = require('express');
+const fs = require('fs');
 const dirTree = require('directory-tree');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+const dir = '/home/baldvegeta/Desktop/hello';
+const Tree  = dirTree(dir);
+
+app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-    res.status(200).send('<h1>Send request to <a href="http://localhost:3000/data">http://localhost:3000/data</a></h1>')
+    res.status(200).send('<h1>Send request to <a href="http://localhost:3000/data">http://localhost:4040/data</a></h1>')
 });
 
 app.get('/data', (req, res) => {
-    res.status(200).json(dirTree('/home/baldvegeta/Desktop/hello'));
+    res.status(200).json(Tree);
+});
+
+app.post('/filedata', (req, res) => {
+    const { filepath } = req.body;
+    fs.readFile(filepath, (err, data) => {
+        let filedata = data.toString();
+        if(err) {
+            res.status(404).json({
+                success: false
+            });
+        }
+        res.status(200).json({
+            success: true,
+            filedata
+        });
+    });
 });
 
 app.listen(4040, () => {
